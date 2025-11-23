2320a3d7101c85812b7817dfc3a4ed6b36a1e69f
송찬의 <dforce103@gmail.com>
2025-11-23 19:38:30 +0900

    t8:blue

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -4,33 +4,15 @@ public class Calc {
     public static int run(String exp) {
         String[] bits = exp.split(" ");
 
-        int num1 = Integer.parseInt(bits[0]);
-        String op = bits[1];
-        int num2 = Integer.parseInt(bits[2]);
+        int result = Integer.parseInt(bits[0]);
 
-        int result = 0;
-        if (op.equals("+")) {
-            result = num1 + num2;
-        } else if (op.equals("-")) {
-            result = num1 - num2;
+        for (int i = 1; i < bits.length; i += 2) {
+            String op = bits[i];
+            int num = Integer.parseInt(bits[i + 1]);
+
+            if (op.equals("+")) result += num;
+            else if (op.equals("-")) result -= num;
         }
-
-        if (bits.length == 3) {
-            return result;
-        }
-
-        String op2 = bits[3];
-        int num3 = Integer.parseInt(bits[4]);
-
-        if (op2.equals("+")) {
-            result = result + num3;
-        } else if (op2.equals("-")) {
-            result = result - num3;
-        }
-
         return result;
-
-
-
     }
 }
